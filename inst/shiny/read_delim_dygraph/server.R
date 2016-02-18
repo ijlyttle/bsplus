bb255ba8592c2e81548907df7b25d3ed4d52d645
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2016-02-18 11:37:01 -0600

    first test of read_delim as accordion

--- inst/shiny/read_delim_dygraph/server.R
+++ inst/shiny/read_delim_dygraph/server.R
@@ -1,9 +1,17 @@
 library("shiny")
 library("shinyjs")
-library("bsplus")
+library("shinypod")
+library("dygraphs")
 
 shinyServer(function(input, output, session) {
 
-  disable("set")
+  rct_data <- callModule(read_delim_server, "csv")
+
+  rct_dyg <- callModule(dygraph_server, "dyg", data = rct_data)
+
+  output$csv_dyg <- renderDygraph({
+    rct_dyg() %>%
+      dyOptions(useDataTimezone = TRUE)
+  })
 
 })
--- inst/shiny/read_delim_dygraph/ui.R
+++ inst/shiny/read_delim_dygraph/ui.R
@@ -1,11 +1,16 @@
 library("shiny")
 library("shinyjs")
+library("shinypod")
+library("shinythemes")
+library("shinypod")
 library("bsplus")
+library("dygraphs")
 
 tagList(
   includeCSS(system.file(file.path("css", "omnibus.css"), package = "bsplus")),
   useShinyjs(),
   navbarPage(
+    theme = shinytheme("flatly"),
     title = "Parse CSV & Dygraph",
     windowTitle = "Parse CSV & Dygraph",
     tabPanel(
@@ -13,19 +18,19 @@ tagList(
       collapse_sidebar_set(
         id_set = "set",
         bstype_open = "primary",
-        bstype_closed = "success",
+        bstype_closed = "info",
         collapse_sidebar_layout(
           id_layout = "layout_1",
           collapse_sidebar_panel =
             collapse_sidebar_panel(
-              title = "title 1",
-              "content 1"
+              title = "Parse CSV",
+              read_delim_sidebar_side("csv")
             ),
           collapse_main_panel =
             collapse_main_panel(
               bs_panel(
                 use_bstype_open = TRUE,
-                "main content 1"
+                read_delim_sidebar_main("csv")
               )
             )
         ),
@@ -33,14 +38,14 @@ tagList(
           id_layout = "layout_2",
           collapse_sidebar_panel =
             collapse_sidebar_panel(
-              title = "title 2",
-              "content 2"
+              title = "Dygraph",
+              dygraph_sidebar_side("dyg")
             ),
           collapse_main_panel =
             collapse_main_panel(
               bs_panel(
                 use_bstype_open = TRUE,
-                "main content 2"
+                dygraphOutput("csv_dyg")
               )
             )
         )
