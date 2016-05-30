eeb0df98caa2071c9ef1f16282e7c9acb8937f86
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2016-05-30 15:16:50 -0500

    updating

--- R/embed_video.R
+++ R/embed_video.R
@@ -1,24 +1,26 @@
-#' embed_vimeo
-#'
 #' creates a centered div for embedding a video from vimeo
 #'
-#' @param vimeo_id character for vimeo_id
+#' @param id       character, vimeo_id
+#' @param height   numeric, height of iframe (pixels)
+#' @param width    numeric, width of iframe (pixels)
 #'
 #' @return html code describing div
+#' @examples
+#' embed_vimeo(45196609)
 #'
 #' @export
 #
-embed_vimeo <- function(vimeo_id){
+embed_vimeo <- function(id, width = 500, height = 281){
 
   url_vimeo <-
-    paste0("https://player.vimeo.com/video/", vimeo_id)
+    paste0("https://player.vimeo.com/video/", as.character(id))
 
   htmltools::tags$div(
     align = "center",
     htmltools::tags$iframe(
       src = url_vimeo,
-      width = "500",
-      height = "281",
+      width = as.character(width),
+      height = as.character(height),
       frameborder = 0,
       webkitallowfullscreen = "",
       mozallowfullscreen = "",
@@ -27,3 +29,34 @@ embed_vimeo <- function(vimeo_id){
   )
 
 }
+
+#' creates a centered div for embedding a video from YouTube
+#'
+#' @param id character, id
+#' @param height   numeric, height of iframe (pixels)
+#' @param width    numeric, width of iframe (pixels)
+#'
+#' @return html code describing div
+#' @examples
+#' embed_youtube("dQw4w9WgXcQ")
+#'
+#' @export
+#
+embed_youtube <- function(id, width = 500, height = 281){
+
+  url_youtube <-
+    paste0("https://www.youtube.com/embed/", as.character(id))
+
+  htmltools::tags$div(
+    align = "center",
+    htmltools::tags$iframe(
+      src = url_youtube,
+      width = as.character(width),
+      height = as.character(height),
+      frameborder = 0,
+      allowfullscreen = ""
+    )
+  )
+
+}
+
