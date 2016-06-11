089d02d550f84279df16c1f2ecd9d9075a470dbe
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2016-06-11 14:26:28 -0500

    rework collapse

--- R/collapse.R
+++ R/collapse.R
@@ -1,68 +1,42 @@
 #' Creates a collapsible div
 #'
-#' Important to note that it is your responsibility to ensure that id is unique
+#' It is your responsibility to ensure that id is unique
 #' among html elements in your page. If you have non-unique id's, strange things may
 #' happen as your page is rendered.
 #'
-#' @param id      character, unique id for collapsible div
+#' @param id       character, unique id for collapsible div
+#' @param content  htmltools \code{\link[htmltools]{tagList}}
 #'
 #' @return \code{\link[htmltools]{tag}} div
 #' @examples
-#' collapse(id = "id_yeah")
+#' library("htmltools")
+#' collapse(id = "id_yeah", tags$p("Yeah Yeah Yeah"))
 #'
 #' @family collapse functions
 #' @seealso \url{https://getbootstrap.com/javascript/#collapse}
 #'
 #' @export
 #
-collapse <- function(id){
+collapse <- function(id, content = NULL){
 
-  tag <- htmltools::tags$div(class = "collapse", id = id)
+  tag <- htmltools::tags$div(class = "collapse", id = id, content)
 
   tag
 }
 
-#' Appends a tag to a collapsible div
-#'
-#' This is a thin wrapper to htmltools \code{\link[htmltools]{tagAppendChild}}
-#'
-#' @param tag_collapse \code{\link[htmltools]{tag}}, made using \code{\link{collapse()}}
-#' @param tag_child \code{\link[htmltools]{tag}} to be appended to \code{tag_collapse}
-#'
-#' @return \code{\link[htmltools]{tag}}, modified copy of \code{tag_collapse}
-#' @examples
-#' collapse(id = "id_yeah") %>%
-#'   collapse_append("Yeah Yeah Yeah")
-#'
-#' @family collapse functions
-#' @seealso \url{https://getbootstrap.com/javascript/#collapse}
-#'
-#' @export
-#
-collapse_append <- function(tag_collapse, tag_child){
-
-  tag_collapse <- .tag_validate(tag_collapse, name = "div")
-
-  tag_child <- .tag_validate(tag_child)
-
-  tag_collapse <- htmltools::tagAppendChild(tag_collapse, tag_child)
-
-  tag_collapse
-}
-
 #' Attaches a collapsible div to a control
 #'
-#' Works on either a button (\code{<button>}) or a link (\code{<a>}).
+#' Typical to use either a button (\code{<button>}) or a link (\code{<a>}).
 #'
 #' You may attach a collapseable div to more than one control.
 #'
 #' @param tag          htmltools \code{\link[htmltools]{tag}},
-#'  must be a link or a button
 #' @param id_collapse  character, id for the collapsible div
 #'
 #' @return \code{\link[htmltools]{tag}}, modified copy of \code{tag}
 #' @examples
-#' htmltools::tags$button("She Loves You") %>%
+#' library("htmltools")
+#' tags$button("She Loves You") %>%
 #'   collapse_attach("id_yeah")
 #'
 #' @family collapse functions
@@ -72,22 +46,20 @@ collapse_append <- function(tag_collapse, tag_child){
 #
 attach_collapse <- function(tag, id_collapse){
 
-  tag <- .tag_validate(tag, name = c("a", "button"))
+  tag <- .tag_validate(tag)
 
   tag <- append_bsopts(tag, toggle = "collapse")
 
-  # button
-  if (identical(tag$name, "button")){
-    tag <- append_bsopts(tag, target = .id(id_collapse))
-  }
-
-  # link
   if (identical(tag$name, "a")){
+    # link
     tag <- htmltools::tagAppendAttributes(
       tag,
       role = "button",
       href = .id(id_collapse)
     )
+  } else {
+    # not a link
+    tag <- append_bsopts(tag, target = .id(id_collapse))
   }
 
   tag
--- tests/testthat/test-collapse.R
+++ tests/testthat/test-collapse.R
@@ -10,25 +10,26 @@ test_button <- tags$button()
 
 test_that("attach_collapse throws correct errors", {
   expect_error(attach_collapse(x, id_collapse = "foo"), regexp = "shiny")
-  expect_error(attach_collapse(test_div, id_collapse = "foo"), regexp = "button")
 })
 
 result_button <- attach_collapse(test_button, id_collapse = "foo")
 test_that("attach_collapse button conforms", {
-  expect_equal(tagGetAttribute(result_button, "data-toggle"), "collapse")
-  expect_equal(tagGetAttribute(result_button, "data-target"), "#foo")
+  expect_identical(tagGetAttribute(result_button, "data-toggle"), "collapse")
+  expect_identical(tagGetAttribute(result_button, "data-target"), "#foo")
 })
 
 result_link <- attach_collapse(test_link, id_collapse = "foo")
 test_that("attach_collapse link conforms", {
-  expect_equal(tagGetAttribute(result_link, "data-toggle"), "collapse")
-  expect_equal(tagGetAttribute(result_link, "role"), "button")
-  expect_equal(tagGetAttribute(result_link, "href"), "#foo")
+  expect_identical(tagGetAttribute(result_link, "data-toggle"), "collapse")
+  expect_identical(tagGetAttribute(result_link, "role"), "button")
+  expect_identical(tagGetAttribute(result_link, "href"), "#foo")
 })
 
-result_target <- collapse(id = "foo")
+content <- tags$p("Yo")
+result_target <- collapse(id = "foo", content = content)
 test_that("collapse div conforms", {
-  expect_equal(result_target$name, "div")
-  expect_equal(tagGetAttribute(result_target, "class"), "collapse")
-  expect_equal(tagGetAttribute(result_target, "id"), "foo")
+  expect_identical(result_target$name, "div")
+  expect_identical(tagGetAttribute(result_target, "class"), "collapse")
+  expect_identical(tagGetAttribute(result_target, "id"), "foo")
+  expect_identical(result_target$children, list(content))
 })
