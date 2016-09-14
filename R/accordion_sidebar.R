5ced0d5bab353c6260fc410e240533c19c4b85b9
Ian Lyttle <ian.lyttle@schneider-electric.com>
2016-09-14 09:30:53 -0500

    adds shell for accordion_sidebar

---
+++ R/accordion_sidebar.R
@@ -0,0 +1,22 @@
+#' framework for accordion-sidebar
+#'
+#' @param id              character, id for div and root it for included elements
+#' @param width_side      numeric, number of bootstrap columns for sidebar panels
+#' @param width_main      numeric, number of bootstrap columns for main panels
+#' @param position        character, indicates where to put the sidebar panels with
+#'    repspect to the main panels
+#'
+#' @return \code{bsplus_accsidebar} object
+#'
+#' @examples
+#' bs_accordion_sidebar(id = "meet_the_beatles")
+#'
+#' @export
+#'
+bs_accordion_sidebar <- function(id, width_side = 4, width_main = 8,
+                                 position = c("left", "right")){
+
+
+
+
+}
