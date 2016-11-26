d191bc23df77e2f59cc112a6855b889ee5bcecde
Ian Lyttle <ian.lyttle@schneider-electric.com>
2016-11-26 16:54:54 -0600

    more trimming

--- R/utils.R
+++ R/utils.R
@@ -46,35 +46,6 @@ NULL
   invisible(tag)
 }
 
-# if a tag has an attribute `use-bstype-open` set to true,
-# replace the class "panel-whatever
-set_bstype_open <- function(
-  tag,
-  class,
-  bstype_open = c("primary", "success", "info", "warning", "danger", "default")
-){
-
-  #args
-  bstype_open = match.arg(bstype_open)
-
-  bstype <- c("primary", "success", "info", "warning", "danger", "default")
-  regex <- paste0("(", class, "-", bstype, ")", collapse = "|")
-  class_sub <- paste(class, bstype_open, sep = "-")
-
-  if (identical(htmltools::tagGetAttribute(tag, "use-bstype-open"), "true")){
-
-    panel_bstype_open <- paste("panel", bstype_open, sep = "-")
-
-    class_current <- htmltools::tagGetAttribute(tag, "class")
-
-    class_new <- stringr::str_replace(class_current, regex, class_sub)
-
-    tag$attribs$class <- class_new
-  }
-
-  tag
-}
-
 
 # we will be able to get rid of this code
 get_class <- function(tag){
